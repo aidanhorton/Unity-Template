a24c00071a77e27c575b5d6bb358b1942505d729
Aidan Horton <aidan.horton@virginmedia.com>
2022-07-13 15:41:53 +0100

    Add state machine + Other settings

---
+++ Assets/_Scripts/Utilities/State.cs
@@ -0,0 +1,17 @@
+public abstract class State
+{
+    public virtual void OnEnterState()
+    {
+        
+    }
+    
+    public virtual void OnUpdate()
+    {
+        
+    }
+
+    public virtual void OnEndState()
+    {
+        
+    }
+}
